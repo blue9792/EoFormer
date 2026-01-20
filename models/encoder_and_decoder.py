b8d9b9869b50d5e0d15d8207b6bff87c33290316
blue9792 <106809576+blue9792@users.noreply.github.com>
2026-01-20 16:16:47 +0800

    Add position_embedding arg to Block

--- models/encoder_and_decoder.py
+++ models/encoder_and_decoder.py
@@ -235,6 +235,7 @@ class Block(nn.Module):
                  drop=0., drop_path=0.,
                  layer_scale_init_value=None, res_scale_init_value=None,
                  scale_trainable=True, 
+                 position_embedding=None,
                  ):
 
         super().__init__()
@@ -254,6 +255,7 @@ class Block(nn.Module):
             if layer_scale_init_value else nn.Identity()
         self.res_scale2 = Scale(dim=dim, init_value=res_scale_init_value) \
             if res_scale_init_value else nn.Identity()
+        self.position_embedding = position_embedding
         
     def forward(self, x):
         if self.position_embedding:
